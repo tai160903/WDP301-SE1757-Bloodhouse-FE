87087374202d985b6e7f399fe14ab5997f34faa1
Shin <phanhoangnam17@gmail.com>
2025-06-22 23:02:01 +0700

    update blood cp

---
+++ src/services/bloodCompatibility/index.ts
@@ -0,0 +1,26 @@
+import { instance } from "../instance";
+
+export interface BloodCompability {
+  _id: string;
+  bloodCompability: string;
+  componentId: string;
+  canDonateTo:[],
+  canReceiveFrom:[],
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
+
+export interface BloodCompabilityResponse extends IResponse.Response {
+  data: BloodCompability[];
+}
+
+
+export const getBloodCompabilities = async (  bloodGroupId : string,  componentId: string ): Promise<BloodCompability[]> => {
+  try {
+    const { data } = await instance.get<BloodCompabilityResponse>(`/blood-compability/${bloodGroupId}/${componentId}`);
+    return data.data;
+  } catch (error: any) {
+    throw new Error(error?.response?.data?.message || "Lỗi khi gọi API.");
+  }
+};
